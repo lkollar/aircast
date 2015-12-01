16ad33300846849929b28fd59329a086c26abe99
Laszlo Kiss-Kollar <kiss.kollar.laszlo@gmail.com>
2015-12-01 17:53:02 +0000

    Randomised HTTP port or specify on CLI

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ var BufferedStream = require('bufferedstream');
 var wav = require('wav');
 var cast = require('./cast');
 var os = require('os');
-
 var audioStream = new BufferedStream;
+
 var serverName = 'AirCast Server';
 
-http.createServer(function (req, res) {
+var optimist = require('optimist')
+    .usage('Usage $0 [options]')
+    .describe('h', 'Display this help')
+    .describe('p', 'Port for HTTP server')
+    .alias('h', 'help')
+    .alias('p', 'port')
+    .default('port', 0);
+
+var argv = optimist.argv;
+if (argv.help) {
+    optimist.showHelp();
+    process.exit(0);
+}
+
+var server = http.createServer(function (req, res) {
     logger.info('HTTP Client connected. Headers: ', req.headers);
     var writer = wav.Writer();
     res.writeHead(200, {
@@ -17,7 +31,28 @@ http.createServer(function (req, res) {
     });
 
     audioStream.pipe(writer).pipe(res);
-}).listen(1337);
+});
+
+server.on('error', function(e) {
+    logger.info(e.code);
+    switch (e.code) {
+        case 'EACCES':
+            logger.error('Invalid port');
+            process.exit(-1);
+            break;
+        default:
+            logger.error('HTTP server error: ', e);
+            process.exit(-1);
+            break;
+    }
+});
+
+var http_port;
+server.listen(argv.port);
+server.on('listening', function() {
+    http_port = server.address().port;
+    logger.info('HTTP server listening on port', http_port);
+});
 
 
 function getIp(hostName) {
