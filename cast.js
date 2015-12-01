497797fe2762af143c4163f2ce9d902a739a285c
Laszlo Kiss-Kollar <kiss.kollar.laszlo@gmail.com>
2015-12-01 14:57:27 +0000

    Log with winston instead of console.log

--- cast.js
+++ cast.js
@@ -1,3 +1,4 @@
+var logger = require('winston');
 var Client = require('castv2-client').Client;
 var DefaultMediaReceiver  = require('castv2-client').DefaultMediaReceiver;
 var mdns = require('mdns');
@@ -6,9 +7,14 @@ function Sender(mediaUrl) {
     this.mediaUrl_ = mediaUrl;
     this.browser_ = mdns.createBrowser(mdns.tcp('googlecast'));
     this.browser_.on('serviceUp', function(service) {
-        console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
+        logger.log(
+            'info',
+            'Found Chromecast device "%s" at %s:%d',
+            service.name,
+            service.addresses[0],
+            service.port);
+
         deviceUp(service.addresses[0], mediaUrl);
-        //this.browser_.stop();
     });
 
     function deviceUp(host, mediaUrl) {
@@ -17,7 +23,7 @@ function Sender(mediaUrl) {
 
         client.connect(host, function () {
 
-            console.log('connected, launching app ...');
+            logger.info('Connected to Chromecast device, launching app...');
             client.launch(DefaultMediaReceiver, function (err, player) {
                 var media = {
                     contentId: mediaUrl,
@@ -34,26 +40,14 @@ function Sender(mediaUrl) {
                         //]
                     }
                 };
-                console.log('Playing media: ' + JSON.stringify(media));
+                logger.info('Opening media: ', media);
+
                 player.on('status', function (status) {
-                    //console.log('status broadcast playerState=%s', JSON.stringify(status));
-                    console.log('player status');
+                    logger.log('debug', 'Sender status:', status);
                 });
 
-                //console.log('app "%s" launched, loading media %s ...', player.session.displayName, media.contentId);
-                console.log('app launched');
-
                 player.load(media, {autoplay: true}, function (err, status) {
-                    //console.log('media loaded playerState=%s', status.playerState);
-                    console.log('media loaded');
-
-                    // Seek to 2 minutes after 15 seconds playing.
-                    //setTimeout(function () {
-                    //    player.seek(2 * 60, function (err, status) {
-                    //        //
-                    //    });
-                    //}, 15000);
-
+                    logger.log('info', 'Media loaded - status:', status);
                 });
 
             });
@@ -61,7 +55,7 @@ function Sender(mediaUrl) {
         });
 
         client.on('error', function(err) {
-            console.log('Error: %s', err.message);
+            logger.warn('Error: ', err);
             client.close();
         });
     }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var logger = require('winston');
 var AirTunesServer = require('nodetunes');
 var http = require('http');
 var BufferedStream = require('bufferedstream');
@@ -7,21 +8,19 @@ var cast = require('./cast');
 var audioStream = new BufferedStream;
 
 var server = http.createServer(function (req, res) {
-    console.log('HTTP Client connected');
+    logger.info('HTTP Client connected. Headers: ', req.headers);
     var writer = wav.Writer();
     res.writeHead(200, {
         'Content-Type': 'audio/wav'
     });
 
     audioStream.pipe(writer).pipe(res);
-});
+}).listen(1337);
 
-
-server.listen(1337);
 
 var myIp = '10.0.1.48';
 //var myIp = require('dns').lookup(require('os').hostname(), function (_, add) {
-//    console.log('Local IP: ' + add);
+//    logger.info('Local IP: ', add);
 //    return add;
 //}).hostname;
 
@@ -30,7 +29,7 @@ var airplayServer = new AirTunesServer({
 });
 
 airplayServer.on('clientConnected', function (stream) {
-    console.log('Client connected');
+    logger.info('AirPlay client connected, starting Chromecast receiver...');
     var sender = new cast.Sender('http://' + myIp + ':1337');
     sender.start();
     stream.pipe(audioStream);
